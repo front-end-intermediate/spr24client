4c74596e2d8daf6b67b22e38637e13694e54e0bc
DannyBoyNYC <daniel.deverell@gmail.com>
2024-04-19 13:50:17 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [recipes, setRecipes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(`/api/recipes`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello from App</p>
     </div>
   );
 }
