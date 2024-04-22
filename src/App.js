fc782aaf10c896ecbe163f303f56f62ceea000ea
DannyBoyNYC <daniel.deverell@gmail.com>
2024-04-22 07:20:57 -0400

    done client

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Recipes from "./Recipes";
+import RecipeDetail from "./RecipeDetail";
 
 function App() {
   const [recipes, setRecipes] = React.useState([]);
 
   React.useEffect(() => {
     fetch(`/api/recipes`)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  });
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject(response);
+        }
+      })
+      .then((data) => setRecipes(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div>
-      <p>Hello from App</p>
-    </div>
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Recipes recipes={recipes} />} />
+          <Route
+            path="/:recipeId"
+            element={<RecipeDetail recipes={recipes} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
