63b49936cb0820d218e95bdeb3ec8341aa64b307
DannyBoyNYC <daniel.deverell@gmail.com>
2024-04-23 17:11:01 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,18 +1,94 @@
 import React from "react";
+import Recipes from "./Recipes";
+import RecipeDetail from "./RecipeDetail";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useFetch } from "./hooks/useFetch";
+import Nav from "./Nav";
+import useToggle from "./hooks/useToggle";
 
 function App() {
   const [recipes, setRecipes] = React.useState([]);
+  const [loggedin, setLoggedin] = useToggle(true);
+  const [loading, setLoading] = useToggle(true);
+  const [error, setError] = React.useState("");
+  const { get, post, del, put } = useFetch(`/api/recipes`);
 
+  const addRecipe = (recipe) => {
+    post("/api/recipes", recipe).then((data) => {
+      setRecipes([data, ...recipes]);
+    });
+  };
+
+  const deleteRecipe = (recipeId) => {
+    console.log("recipeId:", recipeId);
+    del(`/api/recipes/${recipeId}`).then(
+      setRecipes((recipes) =>
+        recipes.filter((recipe) => recipe._id !== recipeId)
+      )
+    );
+  };
+
+  const editRecipe = (updatedRecipe) => {
+    console.log(updatedRecipe);
+    put(`/api/recipes/${updatedRecipe._id}`, updatedRecipe).then(
+      get("/api/recipes").then((data) => {
+        setRecipes(data);
+      })
+    );
+  };
+
+  /* eslint-disable react-hooks/exhaustive-deps */
   React.useEffect(() => {
-    fetch(`/api/recipes`)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  });
+    setLoading(true);
+    get("/api/recipes")
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(error);
+      });
+  }, []);
+
+  // if (loading === true) {
+  //   return <p>Loading</p>;
+  // }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
-    <div>
-      <p>Hello from App</p>
-    </div>
+    <main>
+      <BrowserRouter>
+        <Nav setLoggedin={setLoggedin} loggedin={loggedin} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Recipes
+                recipes={recipes}
+                loggedin={loggedin}
+                addRecipe={addRecipe}
+              />
+            }
+          />
+          <Route
+            path="/:recipeId"
+            element={
+              <RecipeDetail
+                recipes={recipes}
+                deleteRecipe={deleteRecipe}
+                loggedin={loggedin}
+                editRecipe={editRecipe}
+              />
+            }
+          />
+          ;
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
---
+++ src/RecipeDetail.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import FormEditRecipe from "./FormEditRecipe";
+
+function RecipeDetail({ recipes, loggedin, deleteRecipe, editRecipe }) {
+  const { recipeId } = useParams();
+  const [recipeDeleted, setRecipeDeleted] = React.useState(false);
+
+  const currRecipe = recipes.filter((recipe) => recipe._id === recipeId);
+  const thisRecipe = { ...currRecipe[0] };
+
+  const delRecipe = () => {
+    deleteRecipe(thisRecipe._id);
+    setRecipeDeleted(true);
+  };
+
+  if (recipeDeleted) {
+    return (
+      <>
+        <p>Recipe deleted!</p>
+        <Link to="/">Home</Link>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <img src={`/img/${thisRecipe.image}`} alt={thisRecipe.title} />
+      <h1>{thisRecipe.title}</h1>
+      <p>{thisRecipe.description}</p>
+
+      <FormEditRecipe thisRecipe={thisRecipe} editRecipe={editRecipe} />
+      <button onClick={() => deleteRecipe()}>delete</button>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
+
+export default RecipeDetail;
