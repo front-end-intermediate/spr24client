92ec4eebf8881f698bf5fbb80a5cd678df8075fb
DannyBoyNYC <daniel.deverell@gmail.com>
2024-04-23 20:27:07 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Recipes from "./Recipes";
+import RecipeDetail from "./RecipeDetail";
+import { useFetch } from "./hooks/useFetch";
+import useToggle from "./hooks/useToggle";
+import Nav from "./Nav";
 
 function App() {
-  const [recipes, setRecipes] = React.useState([]);
+  const [loggedin, setLoggedin] = useToggle(false);
+  const { loading, data: recipes, error } = useFetch(`/api/recipes`);
 
-  React.useEffect(() => {
-    fetch(`/api/recipes`)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  });
+  if (loading === true) {
+    return <p>Loading</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
-    <div>
-      <p>Hello from App</p>
-    </div>
+    <BrowserRouter>
+      <Nav setLoggedin={setLoggedin} loggedin={loggedin} />
+      <Routes>
+        <Route path="/" element={<Recipes recipes={recipes} />} />
+        <Route path="/:recipeId" element={<RecipeDetail recipes={recipes} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
